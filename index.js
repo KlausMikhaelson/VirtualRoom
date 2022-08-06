32ed5bb98a7ad15242e5ea0a80934cb35231cdd4
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-08-06 05:33:20 +0530

    added socket.io

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require("express")
+const express = require("express");
 const app = express()
+const PORT = process.env.PORT || 3001;
+const server = require("http").createServer(app)
+const io = require("socket.io")(server, { cors: { origin: "*" }})
+
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
-
+console.log("Yup worked")
 
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.listen(3000)
+server.listen(PORT, () => {
+    console.log("server running")
+})
+
+io.on("connection", function(socket) {
+    socket.on('playEvent', function(msg){
+        console.log(msg)
+    })
+    console.log("user conncted" + socket.id);
+})
