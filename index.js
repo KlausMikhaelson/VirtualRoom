8b20854ce6e0038fad17177ef797b78ff0de8ccd
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-08-07 13:45:45 +0530

    added the chat feature

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const io = require("socket.io")(server, { cors: { origin: "*" }})
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
 console.log("Yup worked")
+const users = {}
+
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -22,4 +24,18 @@ io.on("connection", function(socket) {
         io.emit('event', msg)
     })
     console.log("user conncted" + socket.id);
+})
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+      users[socket.id] = name
+      socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+    })
 })
